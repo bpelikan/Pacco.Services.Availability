c2b345886a6099ede7a7450dc1fb8a7ef91b0faa
bpelikan <bartoszpelikan@gmail.com>
2020-03-07 14:52:10 +0100

    Przygotowanie API

--- src/Pacco.Services.Availability.Api/Program.cs
+++ src/Pacco.Services.Availability.Api/Program.cs
@@ -1,20 +1,40 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+﻿using Convey;
+using Convey.Types;
+using Convey.WebApi;
+using Convey.WebApi.CQRS;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Pacco.Services.Availability.Application;
+using Pacco.Services.Availability.Application.Commands;
+using Pacco.Services.Availability.Infrastructure;
+using System.Threading.Tasks;
+
 
 namespace Pacco.Services.Availability.Api
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        public static async Task Main(string[] args)
+            => await CreateWebHostBuilder(args).Build().RunAsync();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureServices(services => services
+                    .AddConvey()
+                    .AddWebApi()
+                    .AddApplication()
+                    .AddInfrastructure()
+                    .Build())
+            .Configure(app => app.UseInfrastructure()
+                .UseRouting()
+                .UseDispatcherEndpoints(endpoints => endpoints
+                    .Get("", ctx => ctx.Response.WriteAsync(
+                            ctx.RequestServices.GetService<AppOptions>().Name))
+                    .Post<AddResource>("resources", afterDispatch : (cmd, ctx) => 
+                        ctx.Response.Created($"resources/{cmd.ResourceId}"))));
+                
     }
 }
