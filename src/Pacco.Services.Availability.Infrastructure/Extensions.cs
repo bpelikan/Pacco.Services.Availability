809ae76c89861f427217be4c2378d876636737f1
bpelikan <bartoszpelikan@gmail.com>
2020-03-07 18:53:06 +0100

    Implementacja repository

--- src/Pacco.Services.Availability.Infrastructure/Extensions.cs
+++ src/Pacco.Services.Availability.Infrastructure/Extensions.cs
@@ -1,5 +1,10 @@
 ﻿using Convey;
+using Convey.Persistence.MongoDB;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Pacco.Services.Availability.Core.Repositories;
+using Pacco.Services.Availability.Infrastructure.Mongo.Documents;
+using Pacco.Services.Availability.Infrastructure.Mongo.Rapositories;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,7 +14,12 @@ namespace Pacco.Services.Availability.Infrastructure
     public static class Extensions
     {
         public static IConveyBuilder AddInfrastructure(this IConveyBuilder builder)
-        { 
+        {
+            builder.Services.AddTransient<IResourcesRepository, ResourcesMongoRepository>();
+
+            builder.AddMongo().
+                AddMongoRepository<ResourceDocument, Guid>("resources");
+
             return builder;
         }
 
--- src/Pacco.Services.Availability.Infrastructure/Mongo/Documents/ResourceDocument.cs
+++ src/Pacco.Services.Availability.Infrastructure/Mongo/Documents/ResourceDocument.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using Convey.Types;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Pacco.Services.Availability.Infrastructure.Mongo.Documents
 {
-    internal sealed class ResourceDocument
+    internal sealed class ResourceDocument : IIdentifiable<Guid>
     {
         public Guid Id { get; set; }
         public int Version { get; set; }
